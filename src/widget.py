f6235d5dbe0e9dbcefaf3d489f3131b62c4253e6
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-10-08 21:27:58 +0500

    add 2 functions

---
+++ src/widget.py
@@ -0,0 +1,18 @@
+from masks import get_mask_account, get_mask_card_number
+
+def mask_account_card(card_info: str) -> str:
+    """ Функция обрабатывает информацию о картах и счетах """
+    if len(card_info.split()[-1]) == 16:
+        card_number_1 = get_mask_card_number(card_info.split()[-1])
+        result = f"{card_info[:-16]}{card_number_1}"
+    elif len(card_info.split()[-1]) == 20:
+        card_number_2 = get_mask_account(card_info.split()[-1])
+        result = f"{card_info[:-20]}{card_number_2}"
+    return result
+
+
+def get_date(date_1: str) -> str:
+    """ Функция возвращает строку с датой в формате 'ДД.ММ.ГГГГ' """
+    date_2, _ = date_1.split('T')
+    year, month, day = date_2.split('-')
+    return f'"{day}.{month}.{year}"'

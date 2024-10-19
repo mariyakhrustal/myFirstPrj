1ae95d459a4cb1d06b1b50b986267b5d957c4081
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-10-19 20:53:04 +0500

    test and fix processing.py

--- src/processing.py
+++ src/processing.py
@@ -12,5 +12,8 @@ def filter_by_state(bank_transaction_data: list[dict[str, Any]], state: str = "E
 
 def sort_by_date(transaction_data: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """Возвращает новый список, в котором исходные словари отсортированы по дате"""
-    sorted_info = sorted(transaction_data, key=lambda x: x["date"], reverse=reverse)
-    return sorted_info
+    if len(transaction_data) < 1:
+        return []
+    else:
+        sorted_info = sorted(transaction_data, key=lambda x: (x["date"], x["id"]), reverse=reverse)
+        return sorted_info

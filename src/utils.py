aac2374762789886ff2b5b031d4b0e2ec97adb8c
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-11-11 23:35:51 +0500

    create func get_operations_data

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+import json
+
+
+def get_operations_data(path: str) -> list:
+    """Получение списков транзакций"""
+    try:
+        with open(path, 'r', encoding='utf-8') as operations_file:
+            try:
+                operations = json.load(operations_file)
+                if isinstance(operations, list):
+                    return operations
+                else:
+                    return []
+            except json.JSONDecodeError:
+                return []
+            except ValueError:
+                return []
+    except FileNotFoundError:
+        return []
+
+
+if __name__ == '__main__':
+    print(get_operations_data('../data/operations.json'))

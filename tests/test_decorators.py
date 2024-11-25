8128c6db8ce00fa7d5725e5dc4f6e6e37628202a
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-11-26 00:25:48 +0500

    test_decorators.py fixes

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 from typing import Any
 
 import pytest
@@ -30,38 +31,42 @@ def test_log_console_error(capsys: pytest.CaptureFixture) -> None:
     assert output.out == "add_func error: TypeError. Inputs: (6, '3'), {}\n"
 
 
-def test_log_directory_creation() -> None:
+def test_log_directory_creation(tmp_path: Path) -> None:
     """Тест на создание директории если её не существует"""
+    log_file = tmp_path / "test.logs"
 
-    @log(filename="logs/mylog.txt")
+    @log(filename=str(log_file))
     def division_func(x: Any, y: Any) -> Any:
         return x / y
 
     division_func(10, 2)
 
-    log_file = "logs/mylog.txt"
     log_dir = os.path.dirname(log_file)
 
     assert os.path.exists(log_dir)
     assert os.path.exists(log_file)
 
 
-def test_log_error_in_file() -> None:
+def test_log_error_in_file(tmp_path: Path) -> None:
     """Тест на успешное выполнение логирования в файл"""
+    log_file = tmp_path / "test.logs"
 
-    @log(filename="logs/mylog.txt")
+    @log(filename=str(log_file))
     def division_func(x: Any, y: Any) -> Any:
         return x / y
 
-    log_file = "logs/mylog.txt"
-    log_dir = os.path.dirname(log_file)
-    if os.path.exists(log_file):
-        os.remove(log_file)
-
     division_func(5, 0)
-    assert os.path.exists(log_file)
     with open(log_file, "r", encoding="utf-8") as file:
         log_content = file.read()
     assert log_content == "division_func error: ZeroDivisionError. Inputs: (5, 0), {}\n"
-    os.remove(log_file)
-    os.rmdir(log_dir)
+
+
+def test_log_create_directory_if_not_exists(tmp_path: Path) -> None:
+    log_file = tmp_path / "logs" / "test.log"
+
+    @log(filename=str(log_file))
+    def division_func(x: Any, y: Any) -> Any:
+        return x / y
+
+    assert division_func(8, 4) == 2
+    assert (tmp_path / "logs").exists()

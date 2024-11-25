320bd3986ec06680bc70e26270bc9ad29011f0ec
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-11-25 13:04:53 +0500

    create funcs to read csv & excel files

--- src/csv_excel_reader.py
+++ src/csv_excel_reader.py
@@ -1,18 +1,20 @@
-import csv
+import pandas as pd
 
 
-def get_csv_transacts(path: str) -> list:
+def get_csv_transacts(path: str) -> list[dict]:
     """Get transactions from csv"""
-    try:
-        with open(path, mode="r", encoding="utf-8") as file:
-            reader = csv.DictReader(file)
-            data = [row for row in reader]
-            if data:
-                return data
-            return []
-    except FileNotFoundError:
-        return []
+    df = pd.read_csv(path)
+    transacts_data = df.to_dict(orient="records")
+    return transacts_data
 
 
-if __name__ == "__main__":
-    print(get_csv_transacts("../data/transactions_csv.csv"))
+def get_excel_transacts(path: str) -> list[dict]:
+    """Get transactions from excel"""
+    df = pd.read_excel(path)
+    transacts_data = df.to_dict(orient="records")
+    return transacts_data
+
+
+# if __name__ == "__main__":
+#     print(get_csv_transacts("data/transactions_csv.csv"))
+#     print(get_excel_transacts("data/transactions_excel.xlsx"))

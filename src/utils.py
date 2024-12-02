c58cff09f331e0e2edfbef33de191d598f82854e
Mariya Khrustaleva <mbotmanova@gmail.com>
2024-12-03 03:12:18 +0500

    utils.py and masks.py fixes

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import os
 
 logs_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "logs")
 if not os.path.exists(logs_dir):
-    os.makedirs(logs_dir)
+    os.makedirs(logs_dir)  # pragma: no cover
 
 
 logger = logging.getLogger("utils")
